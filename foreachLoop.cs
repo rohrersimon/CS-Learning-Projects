c099c8b1935b52ab904f734e8b6819cb41907ffb
Simon Rohrer <simon.rohrer@live.com>
2023-10-18 18:36:55 +0100

    commit

--- foreachLoop.cs
+++ foreachLoop.cs
@@ -13,3 +13,11 @@ Console.WriteLine($"We have {sum} items in inventory.");
 
 string[] orders = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
 
+foreach (string orderID in orders)
+{
+    char firstletter = orderID.First();
+    if(firstletter == 'B')
+    {
+        Console.WriteLine($"{orderID} is at risk of fraud.");
+    }
+}
