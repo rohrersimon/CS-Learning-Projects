2bd8b974e49c67d71d8b871871226b417d4bdf6d
Simon Rohrer <simon.rohrer@live.com>
2023-10-19 12:40:32 +0100

    commit

--- foreachLoop.cs
+++ foreachLoop.cs
@@ -21,3 +21,26 @@ foreach (string orderID in orders)
         Console.WriteLine($"{orderID} is at risk of fraud.");
     }
 }
+
+/*
+    Reverse a string and count the number of times a particular character appears
+*/
+
+string str = "The quick brown fox jumps over the lazy dog.";
+
+char[] charMessage = str.ToCharArray();
+Array.Reverse(charMessage);
+
+int x = 0;
+foreach (char i in charMessage)
+{
+    if (i == 'o')
+    {
+        x++;
+    }
+}
+
+string new_message = new String(charMessage);
+
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");
