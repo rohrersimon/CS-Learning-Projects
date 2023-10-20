6fa5ca6c1b705f27af20d298fb4c9f9889074787
Simon Rohrer <simon.rohrer@live.com>
2023-10-20 16:46:58 +0100

    commit

---
+++ Course1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+public class Course1
+{
+    public static void ExerciseForeach()
+    {
+        int[] inventory = { 200, 450, 700, 175, 250 };
+        int sum = 0;
+        int bin = 0;
+        foreach (int items in inventory)
+        {
+            sum += items;
+            bin++;
+            Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+        }
+        Console.WriteLine($"We have {sum} items in inventory.");
+    }
+
+    public static void ExerciseFraudID()
+    {
+        string[] orders = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+
+        foreach (string orderID in orders)
+        {
+            char firstletter = orderID.First();
+            if(firstletter == 'B')
+            {
+                Console.WriteLine($"{orderID} is at risk of fraud.");
+            }
+        }
+    }
+
+    public static void ExerciseSpacing()
+    {
+        /*
+            Reverse a string and count the number of times a particular character appears
+        */
+
+        string str = "The quick brown fox jumps over the lazy dog.";
+
+        char[] charMessage = str.ToCharArray();
+        Array.Reverse(charMessage);
+
+        int x = 0;
+        foreach (char i in charMessage)
+        {
+            if (i == 'o')
+            {
+                x++;
+            }
+        }
+
+        string new_message = new String(charMessage);
+
+        Console.WriteLine(new_message);
+        Console.WriteLine($"'o' appears {x} times.");
+    }
+}
---
+++ Course2.cs
@@ -0,0 +1,139 @@
+using System;
+using System.Runtime.InteropServices;
+
+public class Course2
+{
+    /*
+    Program that takes student names and their 5 scores. It calculates
+    their overall score and letter grade, and then prints the results
+    to the console.
+    */
+    public static void Module5()
+    {
+        // Initialise variables
+        int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
+        int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
+        int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
+        int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
+
+        string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
+        
+        int[] studentScores = new int[] { 1, 1, 1, 1, 1 };
+        decimal studentScoreSum = 0m;
+        decimal studentScore = 0m;
+        int amountExtraCreditScores = 0;
+        string studentLetterGrade = "Z";
+        int amountRegularScores = 5;
+
+        //Calculations and print to console
+        Console.WriteLine("Student\t\tGrade\n");
+
+        foreach (string name in studentNames)
+        {
+            if (name == "Sophia")
+            {
+                studentScores = sophiaScores;
+            }
+            else if (name == "Andrew")
+            {
+                studentScores = andrewScores;
+            }
+            else if (name == "Emma")
+            {
+                studentScores = emmaScores;
+            }
+            else if (name == "Logan")
+            {
+                studentScores = loganScores;
+            }
+            
+
+            for (int i = amountRegularScores; i >= 1; i--)
+            {
+                studentScoreSum += studentScores[(i - 1)];
+            }
+
+            if (studentScores.Length > amountRegularScores)
+            {
+                amountExtraCreditScores = studentScores.Length - amountRegularScores;
+                for (int i = amountExtraCreditScores; i > 0; i--)
+                {
+                    studentScoreSum += (studentScores[(4 + i)]/10);
+                }
+            }
+
+            studentScore = studentScoreSum/amountRegularScores;
+            //studentScore = (studentScoreSum/studentScores.Length);
+
+            studentLetterGrade = GetLetterGrade(studentScore);
+
+            Console.WriteLine($"{name}:\t\t{studentScore}\t{studentLetterGrade}");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("Press the Enter key to continue");
+        Console.ReadLine();
+    }
+
+    /*
+    This method's argument is the number grade and returns
+    the corresponding letter grade.
+    */
+    public static string GetLetterGrade(decimal grade)
+    {
+        string[] letterGrades = { "A+", "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D+", "D", "D-", "F" };
+
+        if (grade >= 97)
+        {
+            return letterGrades[0];
+        }
+        else if (grade >= 93)
+        {
+            return letterGrades[1];
+        }
+        else if (grade >= 90)
+        {
+            return letterGrades[2];
+        }
+        else if (grade >= 87)
+        {
+            return letterGrades[3];
+        }
+        else if (grade >= 83)
+        {
+            return letterGrades[4];
+        }
+        else if (grade >= 80)
+        {
+            return letterGrades[5];
+        }
+        else if (grade >= 77)
+        {
+            return letterGrades[6];
+        }
+        else if (grade >= 73)
+        {
+            return letterGrades[7];
+        }
+        else if (grade >= 70)
+        {
+            return letterGrades[8];
+        }
+        else if (grade >= 67)
+        {
+            return letterGrades[9];
+        }
+        else if (grade >= 63)
+        {
+            return letterGrades[10];
+        }
+        else if (grade >= 60)
+        {
+            return letterGrades[11];
+        }
+        else
+        {
+            return letterGrades[12];
+        }
+    }
+}
