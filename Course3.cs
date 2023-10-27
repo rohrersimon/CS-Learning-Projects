a0f682f48280c7d2cf45d5620b7c956c27d02998
Simon Rohrer <105282514+rohrersimon@users.noreply.github.com>
2023-10-27 23:51:07 +0100

    Course 3, Module 1

--- Course3.cs
+++ Course3.cs
@@ -2,14 +2,29 @@ using System;
 
 public class Course3
 {
-        public static void Module2()
+    public static void Module3()
+    {
+        Console.WriteLine("test bestanden");
+    }
+    public static void Module2()
     {
         Console.WriteLine("test bestanden");
     }
     public static void Module1()
     {
-        string pangram = "The quick brown fox jumps over the lazy dog.";
-        Console.WriteLine(!pangram.Contains("fox"));
-        Console.WriteLine(!pangram.Contains("cow"));
+        string permission = "Manager|Admin";
+        int level = 56;
+
+        if (permission.Contains("Admin") && level > 55)
+            Console.WriteLine("Welcome, Super Admin user.");
+        else if (permission.Contains("Admin") && level <= 55)
+            Console.WriteLine("Welcome, Admin user.");
+        else if (permission.Contains("Manager") && level >= 20)
+            Console.WriteLine("Contact an Admin for access.");
+        else if (permission.Contains("Manager") && level < 20)
+            Console.WriteLine("You do not have sufficient privileges.");
+        else if (!permission.Contains("Admin") || !permission.Contains("Manager"))
+            Console.WriteLine("You do not have sufficient privileges.");
+        
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,6 +4,6 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Course3.Module1();
+        Course3.Module2();
     }
 }
