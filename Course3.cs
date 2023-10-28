f9e624b077e449abe00910fe2cd1bc7b193603d3
Simon Rohrer <105282514+rohrersimon@users.noreply.github.com>
2023-10-28 21:40:05 +0100

    Course 3, Code Project 2

--- Course3.cs
+++ Course3.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel;
 using System.Globalization;
 
 public class Course3
@@ -15,10 +16,126 @@ public class Course3
     {
         Console.WriteLine("test bestanden");
     }
+
+    public static void CodeProject3()
+    {
+        string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+
+        
+    }
+    public static void CodeProject2()
+    {
+        string? readResult;
+        string trimmedResult;
+        bool validEntry = false;
+
+        Console.WriteLine("Enter your role name (Administrator, Manager, or User):");
+
+        do
+        {
+            readResult = Console.ReadLine();
+            trimmedResult = readResult.Trim().ToLower();
+            readResult = readResult.Trim();
+
+            if (trimmedResult == "administrator" || trimmedResult == "manager" || trimmedResult == "user")
+            {
+                Console.WriteLine($"Your input value ({readResult}) has been accepted.");
+                validEntry = true;
+            }
+            else
+            {
+                Console.WriteLine($"The role name that you entered, \"{readResult}\" is not valid. Enter your role name (Administrator, Manager, or User)");
+            }
+
+        } while (validEntry == false);
+    }
+
+    public static void CodeProject1()
+    {
+        string? readResult;
+        bool validEntry = false;
+        int numericValue = 0;
+
+        Console.WriteLine("Enter an integer value between 5 and 10:");
+
+        do
+        {
+            readResult = Console.ReadLine();
+            validEntry = int.TryParse(readResult, out numericValue);
+
+            if (validEntry)
+            {
+                if (numericValue >= 5 && numericValue <= 10)
+                {
+                    Console.WriteLine($"Your input value ({numericValue}) has been accepted.");
+                }
+                else
+                {
+                    Console.WriteLine($"You entered {numericValue}. Please enter a number between 5 and 10.");
+                    validEntry = false;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Sorry, you entered an invalid number, please try again.");
+            }
+        
+        } while (validEntry == false);
+    }
+
+    public static void Module5b()
+    {
+        int heroHealth = 10;
+        int monsterHealth = 10;
+        bool turn = true; //ture > heroe's turn, false > monster's turn
+        Random attack = new Random();
+
+        do
+        {
+            int hit = attack.Next(1, 11);
+
+            if (turn)
+            {
+                monsterHealth -= hit;
+                turn = false;
+                Console.WriteLine($"Hero hits Monster for {hit}.\nHero health = {heroHealth}\nMonster health = {monsterHealth}\n");
+            }
+            else
+            {
+                heroHealth -= hit;
+                turn = true;
+                Console.WriteLine($"Monster hits Hero for {hit}.\nHero health = {heroHealth}\nMonster health = {monsterHealth}\n");
+            }   
+
+        } while (heroHealth > 0 && monsterHealth > 0);
+            
+        Console.WriteLine(heroHealth > monsterHealth ? "Hero wins!" : "Monster wins!");
+    }
+
+
     public static void Module5()
     {
-        Console.WriteLine("test bestanden");
+        Random random = new Random();
+        int current = random.Next(1, 11);
+
+        do
+        {
+            current = random.Next(1, 11);
+
+            if (current >= 8) continue;
+
+            Console.WriteLine(current);
+        } while (current != 7);
+
+
+        while (current >= 3)
+        {
+            Console.WriteLine(current);
+            current = random.Next(1, 11);
+        }
+        Console.WriteLine($"Last number: {current}");
     }
+
     public static void Module4()
     {
         string fizz = "Fizz";
