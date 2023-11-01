77f5aa831396824ed694cc3273ff4d5745ad68af
Simon Rohrer <simon.rohrer@live.com>
2023-11-01 07:32:35 +0000

    Course 4, Module 9

--- Course4.cs
+++ Course4.cs
@@ -5,7 +5,33 @@ public class Course4
 {
     public static void Module9()
     {
+        decimal price1 = 67.55m;
+        decimal salePrice = 59.99m;
+        string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} price. ", (price1 - salePrice), price1);
+        yourDiscount += $"A discount of {((price1 - salePrice) / price1):P2}!"; //inserted
+        Console.WriteLine(yourDiscount);
 
+        decimal tax = .36785m;
+        Console.WriteLine($"Tax rate: {tax:P2}");
+
+        decimal measurement = 123456.78912m;
+        Console.WriteLine($"Measurement: {measurement:N4} units");
+
+        decimal measurement2 = 123456.78912m;
+        Console.WriteLine($"Measurement: {measurement2:N} units");
+
+        decimal price = 123.45m;
+        int discount = 50;
+        Console.WriteLine($"Price: {price:C} (Save {discount:C})");
+
+        string first = "Hello";
+        string second = "World";
+        Console.WriteLine($"{first} {second}!");
+        Console.WriteLine($"{second} {first}!");
+        Console.WriteLine($"{first} {first} {first}!");
+
+        Console.WriteLine("{1} {0}!", first, second);
+        Console.WriteLine("{0} {0} {0}!", first, second);
     }
     public static void Module8()
     {
